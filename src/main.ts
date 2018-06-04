b2f4a8f6154e27afc4ad712dadba8db06cd8fda6
Treasure <treasurewmj@foxmail.com>
2018-06-05 00:49:53 +0800

    搞定小米云

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,19 @@ import * as Router from 'koa-router'
 import * as bodyParse from 'koa-body'
 import * as responseTime from 'koa-response-time'
 import * as helmet from 'koa-helmet'
+import chalk from 'chalk'
 
 const app = new Koa()
+const router = new Router()
+const log = console.log
 
 // 初始一些
 app.use(responseTime())
 app.use(helmet())
 app.use(bodyParse())
+app.use(router.routes())
+
+const server = app.listen(1314, () => {
+  const address = server.address()
+  log(chalk.green`service is running at ${address['address']}:${address['port']}`)
+})
