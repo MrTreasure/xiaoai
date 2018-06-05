069ccd470f761f820daa4ba63dbfbbb7762ba730
Treasure <treasurewmj@foxmail.com>
2018-06-05 21:09:19 +0800

    修改源添加验证

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,19 @@ const app = new Koa()
 const router = new Router()
 const log = console.log
 
+router.get('/', async (ctx) => {
+  ctx.body = {
+    name: "Hello Xiaoai"
+  }
+})
+
 // 初始一些
 app.use(responseTime())
 app.use(helmet())
 app.use(bodyParse())
 app.use(router.routes())
 
-const server = app.listen(1314, () => {
+const server = app.listen(8080, () => {
   const address = server.address()
   log(chalk.green`service is running at ${address['address']}:${address['port']}`)
 })
